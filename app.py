a5fe68f6d81152fe2e80dea5cbeba7666dd39488
neharika gupta <83262951+neharikagupta12@users.noreply.github.com>
2023-05-25 14:30:38 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,72 +1,83 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Apr 18 10:36:03 2023
 
-@author: POORNIMA
-"""
-
-import streamlit as st 
+import streamlit as st
 from PIL import Image
+import mlxtend
 import pickle
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-# Load the pickled model
-model = pickle.load(open('svmmodel.pkl', 'rb'))
-dataset= pd.read_csv('Social_Network_Ads.csv')
-X = dataset.iloc[:, [2, 3]].values
-from sklearn.preprocessing import StandardScaler
-sc = StandardScaler()
-X = sc.fit_transform(X)
-def predict_note_authentication(UserID, Gender,Age,EstimatedSalary):
-    output= model.predict(sc.transform([[Age,EstimatedSalary]]))
-    print("Purchased", output)
-  if output==[1]:
-    prediction="Item will be purchased"
-  else:
-    prediction="Item will not be purchased"
-  print(prediction)
-  return prediction
-def main():
-    
-    html_temp = """
+from mlxtend.frequent_patterns import apriori, association_rules
+#st.set_option('deprecation.showfileUploaderEncoding',False)
+def find_associatio_rule(support):
+  # Load the pickled model
+  model = pickle.load(open('aprioriexample.pkl','rb'))
+  dataset= pd.read_csv('retail_dataset.csv')    
+  #Create list
+  transactions = []
+  for i in range(0, 315):
+    transactions.append([str(dataset.values[i,j]) for j in range(0, 7)])
+
+  from mlxtend.preprocessing import TransactionEncoder
+  te = TransactionEncoder()
+  te_ary = te.fit(transactions).transform(transactions)
+  df = pd.DataFrame(te_ary, columns=te.columns_)
+  df=df[['Bagel','Bread','Cheese','Diaper','Eggs','Meat','Milk','Pencil','Wine']]
+  freq_items = apriori(df, min_support=support, use_colnames=True)
+  rules = association_rules(freq_items, metric="confidence", min_threshold=0.6)
+  return rules
+
+def find_frequent_items(support):
+  # Load the pickled model
+  model = pickle.load(open('aprioriexample.pkl','rb'))    
+  dataset= pd.read_csv('retail_dataset.csv')
+  #Create list
+  transactions = []
+  for i in range(0, 315):
+    transactions.append([str(dataset.values[i,j]) for j in range(0, 7)])
+
+  from mlxtend.preprocessing import TransactionEncoder
+  te = TransactionEncoder()
+  te_ary = te.fit(transactions).transform(transactions)
+  df = pd.DataFrame(te_ary, columns=te.columns_)
+  df=df[['Bagel','Bread','Cheese','Diaper','Eggs','Meat','Milk','Pencil','Wine']]
+  freq_items = apriori(df, min_support=support, use_colnames=True)
+  rules = association_rules(freq_items, metric="confidence", min_threshold=0.6)
+ 
+  return freq_items
+html_temp = """
    <div class="" style="background-color:blue;" >
-   <div class="clearfix">           
+   <div class="clearfix">          
    <div class="col-md-12">
-   <center><p style="font-size:40px;color:white;margin-top:10px;">Poornima Institute of Engineering & Technology</p></center> 
+   <center><p style="font-size:40px;color:white;margin-top:10px;">Poornima Institute of Engineering & Technology</p></center>
    <center><p style="font-size:30px;color:white;margin-top:10px;">Department of Computer Engineering</p></center>
+   <center><p style="font-size:25px;color:white;margin-top:10px;"Machine Learning Lab Experiment</p></center>
    </div>
    </div>
    </div>
    """
-   st.markdown(html_temp,unsafe_allow_html=True)
-    st.header("Item Purchase Prediction using SVM Algorithm")
-    UserID = st.text_input("UserID","")
-    Gender = st.selectbox(
-    "Gender",
-    ("Male", "Female", "Others")
-    )
-    Age = st.number_input('Insert a Age',18,60)
-    #Age = st.text_input("Age","Type Here")
-    EstimatedSalary = st.number_input("Insert EstimatedSalary",15000,150000)
-    resul=""
-    if st.button("SVM Prediction"):
-      result=predict_note_authentication(UserID, Gender,Age,EstimatedSalary)
-      st.success('SVM Model has predicted {}'.format(result))
-      if st.button("About"):
-      st.header("Developed by shikha gautam")
-      st.subheader("Head , Department of Computer Engineering")
-    html_temp = """
-    <div class="" style="background-color:orange;" >
-    <div class="clearfix">           
-    <div class="col-md-12">
-    <center><p style="font-size:20px;color:white;margin-top:10px;">Machine Learning Experiment : Support Vector Machine and Random Forest</p></center> 
-    </div>
-    </div>
-    </div>
-    """
-    st.markdown(html_temp,unsafe_allow_html=True)
-if __name__=='__main__':
-  main()
-  
-    
+st.markdown(html_temp,unsafe_allow_html=True)
+st.header("Identification of items Purchased together ")
+ 
+#uploaded_file = st.file_uploader("Upload dataset", help='Please upload retail_dataset.csv otherwise leave  blank')
+support = st.number_input('Insert a minimum suppport to find association rule ',0.1,1.1)
+
+ 
+if st.button("Association Rule"):
+  rules=find_associatio_rule(support)
+  st.success('Apriori has found Following rules {}'.format(rules))
+if st.button("Frequent Items"):
+  frequent_items=find_frequent_items(support)
+  st.success('Apriori has found Frequent itemsets {}'.format(frequent_items))      
+if st.button("About"):
+  st.subheader("Developed by Deepak Moud")
+  st.subheader("Head , Department of Computer Engineering")
+html_temp = """
+   <div class="" style="background-color:orange;" >
+   <div class="clearfix">          
+   <div class="col-md-12">
+   <center><p style="font-size:20px;color:white;margin-top:10px;">Machine learning Experiment No. 9</p></center>
+   </div>
+   </div>
+   </div>
+   """
+st.markdown(html_temp,unsafe_allow_html=True)
